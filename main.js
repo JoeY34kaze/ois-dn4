64bfb556e2a6009ad06102781568020fca03a4b5
JožeJerše <ragnar1724@gmail.com>
2014-12-06 18:53:06 +0000

    2

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 
 var baseUrl = 'https://rest.ehrscape.com/rest/v1';
 var queryUrl = baseUrl + '/query';
-
 var username = "ois.seminar";
 var password = "ois4fri";
-
 var ehrIDs=[]
 function getSessionId() {
     var response = $.ajax({
@@ -15,12 +13,8 @@ function getSessionId() {
     });
     return response.responseJSON.sessionId;
 }
-
-
 function kreirajEHRzaBolnika(x, ime, priimek, datum) {
 	var	sessionId = getSessionId();
-
-
 		$.ajaxSetup({ headers: {"Ehr-Session": sessionId}});
 		$.ajax({
 		    url: baseUrl + "/ehr",
@@ -47,9 +41,7 @@ function kreirajEHRzaBolnika(x, ime, priimek, datum) {
 		        });
 		    }
 		});
-	
 }
-
 
 function ustvariBolnike() {
 	var bolnikiUspesni=0;
